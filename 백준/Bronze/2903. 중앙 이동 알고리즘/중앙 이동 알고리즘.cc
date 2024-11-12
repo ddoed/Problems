71c6718b09d1108212684f4c1862efe3509d8492
ddoed <yimnabin20051022@gmail.com>
2024-11-12 21:15:31 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -7,12 +7,10 @@ int main()
 	int n;
 	cin >> n;
 
-	int num1 = 2;
-	int num2;
+	int num = 2;
 	for (int i = 0;i < n-1;i++)
 	{
-		num2 = 2*num1 - 1;
-		num1 = num2;
+		num = 2*num - 1;
 	}
-	cout << (num1 * 2 - 1)*(num1 * 2 - 1);
+	cout << (num * 2 - 1)*(num * 2 - 1);
 }
