1d38c73ac7a5296679a8e1ea54fe93404b9650cf
Nabin Yim <yimnabin20051022@gmail.com>
2025-03-05 21:56:13 +0900

    [Silver IV] Title: 큐, Time: 220 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue<int> q;
+
+void Command(string s)
+{
+	if (s == "push")
+	{
+		int num;
+		cin >> num;
+		q.push(num);
+	}
+	else if (s == "pop")
+	{
+		if (q.empty()) cout << -1 << '\n';
+		else
+		{
+			cout << q.front() << '\n';
+			q.pop();
+		}
+	}
+	else if (s == "size")
+	{
+		cout << q.size() << '\n';
+	}
+	else if (s == "empty")
+	{
+		if (q.empty())
+			cout << 1 << '\n';
+		else
+			cout << 0 << '\n';
+	}
+	else if (s == "front")
+	{
+		if (q.empty()) cout << -1 << '\n';
+		else cout << q.front() << '\n';
+	}
+	else if (s == "back")
+	{
+		if (q.empty()) cout << -1 << '\n';
+		else cout << q.back() << '\n';
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0;i < n;i++)
+	{
+		string s;
+		cin >> s;
+		Command(s);
+	}
+	return 0;
+}
