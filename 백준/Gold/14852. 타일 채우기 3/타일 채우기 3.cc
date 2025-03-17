031f7b53e32c26a998582da6d0472b8617c9d1c3
Nabin Yim <yimnabin20051022@gmail.com>
2025-03-17 20:58:45 +0900

    [Gold IV] Title: 타일 채우기 3, Time: 12 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
+++ 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
@@ -4,24 +4,25 @@ using namespace std;
 
 #define MOD 1000000007
 
-long long dp[1000001][2] = {0};
+long long dp[1000001] = { 0 };
 
 int main()
 {
 	long long n;
 	cin >> n;
 
-	dp[0][0] = 0;
-	dp[1][0] = 2;
-	dp[2][0] = 7;
-	dp[2][1] = 1;
+	dp[0] = 1;
+	dp[1] = 2;
+	dp[2] = 7;
+
+	long long sum = dp[0];
 
 	for (int i = 3;i <= n;i++)
 	{
-		dp[i][1] = (dp[i - 3][0] + dp[i - 1][1]) % MOD;
-		dp[i][0] = (2 * dp[i - 1][0] + 3 * dp[i - 2][0] + 2*dp[i][1]) % MOD;
+		dp[i] = ((dp[i - 1] * 2) % MOD + (dp[i - 2] * 3) % MOD + (sum*2) % MOD) % MOD;
+		sum = (sum + dp[i - 2]) % MOD;
 	}
-	cout << dp[n][0];
+	cout << dp[n];
 
 	return 0;
 }
