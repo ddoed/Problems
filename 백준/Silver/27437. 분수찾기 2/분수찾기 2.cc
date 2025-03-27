2f0d68bdb1f7483b21b81588f0703997dc248a67
Nabin Yim <yimnabin20051022@gmail.com>
2025-03-27 16:19:07 +0900

    [Silver I] Title: 분수찾기 2, Time: 520 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27437. 분수찾기 2/분수찾기 2.cc
+++ 백준/Silver/27437. 분수찾기 2/분수찾기 2.cc
@@ -12,6 +12,7 @@ int main()
 	long long cnt = 0;
 	long long layer = 1;
 
+	// 개수를 더하면서 현재 x의 layer 찾기
 	while (cnt + layer < x)
 	{
 		cnt += layer;
@@ -20,15 +21,17 @@ int main()
 
 	long long left, right;
 
+	// 짝수일 경우 왼쪽 아래에서 오른쪽 위로
 	if (layer % 2 == 0)
 	{
 		left = 0;
 		right = layer + 1;
 		long long r = x - cnt;
+		// 남은 개수만큼 더하고 뺀다
 		left += r;
 		right -= r;
 	}
-	else
+	else // 홀수일 경우 오른쪽 위에서 왼쪽 아래로
 	{
 		left = layer + 1;
 		right = 0;
