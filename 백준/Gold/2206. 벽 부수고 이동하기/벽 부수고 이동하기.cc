eedb79412289f7a23e45add04e65360f1df5f8c5
Nabin Yim <yimnabin20051022@gmail.com>
2025-06-05 11:01:12 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 416 ms, Memory: 86860 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -13,8 +13,7 @@ int dy[4] = { 0, 1, 0, -1 };
 int wMap[1001][1001];
 int n, m;
 bool visited[1001][1001][2];
-queue<tuple<int,int,bool>> q;
-int len = 0;
+queue<tuple<int, int, bool, int>> q;
 
 bool flag = false;
 
@@ -22,33 +21,33 @@ bool isValid(int x, int y) {
 	return x >= 1 && y >= 1 && x <= n && y <= m;
 }
 
-void BFS() {
-	vector<vector<vector<bool>>> visited(n + 1, vector<vector<bool>>(m + 1, vector<bool>(2)));
-	queue<tuple<int, int, bool, int>> q;  // (x, y, 벽부쉈는지, 거리)
-	q.push({ 1, 1, false, 1 });
+void BFS(vector<vector<vector<bool>>>& visited) {
+	q.push({ 1,1,false,1 });
 	visited[1][1][0] = true;
 
 	while (!q.empty()) {
 		auto [x, y, breakWall, dist] = q.front();
 		q.pop();
 
+
 		if (x == n && y == m) {
 			cout << dist;
 			return;
 		}
 
-		for (int j = 0; j < 4; j++) {
+		for (int j = 0;j < 4;j++) {
 			int nxtX = x + dx[j];
 			int nxtY = y + dy[j];
+
 			if (!isValid(nxtX, nxtY)) continue;
 
-			if (wMap[nxtX][nxtY]) { // 벽
+			if (wMap[nxtX][nxtY]) {
 				if (!breakWall && !visited[nxtX][nxtY][1]) {
 					visited[nxtX][nxtY][1] = true;
-					q.push({ nxtX, nxtY, true, dist + 1 });
+					q.push({ nxtX, nxtY, true, dist+1 });
 				}
 			}
-			else { // 길
+			else {
 				if (!visited[nxtX][nxtY][breakWall]) {
 					visited[nxtX][nxtY][breakWall] = true;
 					q.push({ nxtX, nxtY, breakWall, dist + 1 });
@@ -66,8 +65,8 @@ signed main() {
 		string s;
 		cin >> s;
 		for (int j = 0;j < m;j++) {
-			wMap[i][j+1] = s[j] == '1' ? 1 : 0;
+			wMap[i][j + 1] = s[j] == '1' ? 1 : 0;
 		}
 	}
-	BFS();
+	BFS(visited);
 }
