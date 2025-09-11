5b421b9dc4e3fbccf3a4f12276c302f2bceaf0f7
Nabin Yim <yimnabin20051022@gmail.com>
2025-09-11 17:59:40 +0900

    [Gold II] Title: 콘서트, Time: 80 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Gold/34056. 콘서트/콘서트.cc
+++ 백준/Gold/34056. 콘서트/콘서트.cc
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+#define MOD 1000000007
 #define int long long
-
-int n, q;
-vector<long long> D;
-
-void propagate(int start, int dir, long long x) {
-    int idx = start;
-    while (x > 0 && idx >= 1 && idx <= n) {
-        if (D[idx] < x) {
-            long long absorbed = D[idx];
-            D[idx] *= 2;      // 두 배 강화
-            x -= absorbed;    // 남은 소음
-            idx += dir;       // 다음 벽으로 이동
-        } else {
-            D[idx] += x;      // 소음 전부 흡수
-            break;
-        }
-    }
-}
+#define pii pair<int, int>
+#define INF 0x3f3f3f3f
+#define tiii tuple<int,int,int>
+int dx[4] = { 0,-1, 0,1 };
+int dy[4] = { 1, 0, -1, 0 };
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> d(n+1);
 
-    cin >> n;
-    D.resize(n+1);
-    for (int i = 1; i <= n; i++) cin >> D[i];
+	for (int i = 1;i <= n;i++) {
+		cin >> d[i];
+	}
 
-    cin >> q;
-    while (q--) {
-        int t, c;
-        cin >> t >> c;
-        if (t == 1) {
-            long long x;
-            cin >> x;
-            propagate(c, -1, x);   // 왼쪽 전파
-            propagate(c+1, +1, x); // 오른쪽 전파
-        } else {
-            cout << D[c] << "\n";
-        }
-    }
-    return 0;
+	int q;
+	cin >> q;
+	while (q--) {
+		int num, c;
+		cin >> num >> c;
+		if (num == 1) {
+			int x;
+			cin >> x;
+			int noise = x;
+			for (int i = c; i >= 1 && x > 0; i--) {
+				int absorb = min(d[i], x);
+				x -= absorb;
+				d[i] += absorb;
+			}
+			x = noise;
+			for (int i = c + 1; i <= n && x > 0;i++) {
+				int absorb = min(d[i], x);
+				x -= absorb;
+				d[i] += absorb;
+			}
+		}
+		else {
+			cout << d[c] << '\n';
+		}
+	}
+
 }
